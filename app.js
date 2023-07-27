2164e17e75b9fc1b9d88d71f6dc53efd3bb8848a
Hanif <kmuhammadhanif307@gmail.com>
2023-07-27 13:48:49 +0500

    give id and connected with dom and add value

---
+++ app.js
@@ -0,0 +1,6 @@
+function add(){
+    let first=document.getElementById("fn").value
+    let last=document.getElementById("fn").value
+    let output=parseFloat(first)+parent(last)
+    document.getElementById("output").innerHTML=output
+}
